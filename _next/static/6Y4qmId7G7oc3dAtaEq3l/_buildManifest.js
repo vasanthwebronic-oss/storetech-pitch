485cf87a1923db7aa3235794dd05c97ce026024c
muraliwebronic <muralikrishna.webronic@gmail.com>
2025-12-10 16:18:07 +0530

    Updates

---
+++ _next/static/6Y4qmId7G7oc3dAtaEq3l/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/storetech-pitch//_next/:path+",
+        "destination": "/storetech-pitch/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
